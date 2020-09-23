415c58a80851feee95e25256aedb3fcf69d9c925
NathanielCaron <nathaniel.caron22@gmail.com>
2020-09-23 19:34:03 -0400

    Created QuartoPlayerAgent.java class

---
+++ QuartoPlayerAgent.java
@@ -0,0 +1,50 @@
+public class QuartoPlayerAgent extends QuartoAgent {
+
+    //Example AI
+    public QuartoPlayerAgent(GameClient gameClient, String stateFileName) {
+        // because super calls one of the super class constructors(you can overload constructors), you need to pass the parameters required.
+        super(gameClient, stateFileName);
+    }
+
+    public static void main(String[] args) {
+        //start the server
+        GameClient gameClient = new GameClient();
+
+        String ip = null;
+        String stateFileName = null;
+        //IP must be specified
+        if(args.length > 0) {
+            ip = args[0];
+        } else {
+            System.out.println("No IP Specified");
+            System.exit(0);
+        }
+        if (args.length > 1) {
+            stateFileName = args[1];
+        }
+
+        gameClient.connectToServer(ip, 4321);
+        QuartoSemiRandomAgent quartoAgent = new QuartoSemiRandomAgent(gameClient, stateFileName);
+        quartoAgent.play();
+
+        gameClient.closeConnection();
+
+    }
+
+    /*
+	 * This code will try to find a piece that the other player can't use to win immediately
+	 */
+    @Override
+    protected String pieceSelectionAlgorithm() {
+        
+    }
+
+    /*
+     * Do Your work here
+     * The server expects a move in the form of:   row,column
+     */
+    @Override
+    protected String moveSelectionAlgorithm(int pieceID) {
+        
+    }
+}
